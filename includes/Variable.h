0b6935640d942522511932df9f105dd21ed84019
Ben Messerly <bamesserly@gmail.com>
2020-12-10 17:43:18 -0600

    Overhauling Variable class.
    Variable is becoming VariableBase, the new Variable inherits from VariableBase,
    and everything will be a Variable. No more HadronVariable.
    
    The new setup should allow me to use any CVUniverse function as its GetValue
    function, not just one that takes either 0 args or 1 int.
    
    This is needed because the new hadronic energy function is going to take
    a vector of ints that comes from outside of the CVUniverse class. The
    alternative would have been to add the vector to the CVUniverse class, but
    I have a problem with that for a few reasons.
    
    Here I commit just the new classes. Thus breaking, all my analysis code.
    Next, I'll need to update the CVUniverse getters that I want to use for
    Variables to all take an empty arg. Then update my scripts.

--- includes/Variable.h
+++ includes/Variable.h
@@ -2,84 +2,35 @@
 #define Variable_h
 
 #include <functional>
-#include "TArrayD.h"
-#include "CVUniverse.h"
-#include "Histograms.h"
+#include "VariableBase.h"
 
+namespace none
+{
+  struct empty {};
+}
 
-#ifndef __CINT__ // CINT doesn't know about std::function
-class Variable {
-  private:
-    typedef std::function<double(const CVUniverse&)> PointerToCVUniverseFunction;
-    PointerToCVUniverseFunction m_pointer_to_GetValue;
+#ifndef __CINT__
+template<typename T = none::empty>
+class Variable : public VariableBase {
+ private:
+  typedef std::function<double(const CVUniverse&, T)> PointerToCVUniverseFunctionArgs;
+  PointerToCVUniverseFunctionArgs m_pointer_to_GetValueArgs;
 
-  public:
-    //==========================================================================
-    // Constructors
-    //==========================================================================
-    Variable();
+ public: 
+  Variable() {}
 
-    Variable(const std::string label, const std::string xaxis, 
-             const std::string units,
-             const int nbins, const double xmin, const double xmax,
-             PointerToCVUniverseFunction p = &CVUniverse::GetDummyVar,
-             const bool is_true = false);
+  template<typename ...ARGS>
+  Variable(PointerToCVUniverseFunctionArgs p, ARGS... args);
 
-    Variable(const std::string label, const std::string xaxis, 
-             const std::string units,
-             const TArrayD& bins_array,
-             PointerToCVUniverseFunction p = &CVUniverse::GetDummyVar,
-             const bool is_true = false);
+  T m_args;
 
-    //==========================================================================
-    // Data members
-    //==========================================================================
-    std::string m_label;
-    std::string m_units;
-    Histograms  m_hists;
-    bool m_is_true;
-    // also, a pointer to CV universe function for Getting value (private)
+  void SetGetValueArgs(const T& args);
 
-    //==========================================================================
-    // Functions
-    //==========================================================================
-    // Access
-    std::string Name() const { return m_label; }
-    int NBins() const { return m_hists.NBins(); }
-    int XMin() const  { return m_hists.XMin(); }
-    int XMax() const  { return m_hists.XMax(); }
-
-    // Get the variable's value
-    virtual double GetValue (const CVUniverse& universe, const int hadron_ID = -1) const;
-
-    // Histogram Initialization
-    template<typename T>
-    void InitializeAllHists(T systematic_univs, T systematic_univs_truth);
-    template<typename T>
-    void InitializeSidebandHists(T systematic_univs);
-    void InitializeStackedHists();
-    void InitializeDataHists();
-
-    // Write and Load MC Hists to/from a file
-    void WriteMCHists(TFile& fout) const;
-    void LoadDataHistsFromFile(TFile& fin);
-    void LoadMCHistsFromFile(TFile& fin, UniverseMap& error_bands);
-
-
-    // Histogram Access
-    template <typename T>
-    PlotUtils::MnvH1D* GetStackComponentHist(T type) const;
-
-    template <typename T>
-    TObjArray GetStackArray(T type) const ;
-
-    // Get Histograms from File
-    //void GetMCHists(TFile& fin);
+  virtual double GetValue (const CVUniverse& universe) const;
 };
+
 #endif // __CINT__
 
-
 #include "Variable.cxx"
-
 
 #endif // Variable_h
---
+++ includes/VariableBase.h
@@ -0,0 +1,84 @@
+#ifndef VariableBase_h
+#define VariableBase_h
+
+#include <functional>
+#include "TArrayD.h"
+#include "CVUniverse.h"
+#include "Histograms.h"
+
+
+#ifndef __CINT__ // CINT doesn't know about std::function
+class VariableBase {
+ private:
+  typedef std::function<double(const CVUniverse&)> PointerToCVUniverseFunction;
+  PointerToCVUniverseFunction m_pointer_to_GetValue;
+
+ public:
+  //==========================================================================
+  // Constructors
+  //==========================================================================
+  VariableBase();
+
+  VariableBase(const std::string label, const std::string xaxis, 
+           const std::string units,
+           const int nbins, const double xmin, const double xmax,
+           PointerToCVUniverseFunction p = &CVUniverse::GetDummyVar,
+           const bool is_true = false);
+
+  VariableBase(const std::string label, const std::string xaxis, 
+           const std::string units,
+           const TArrayD& bins_array,
+           PointerToCVUniverseFunction p = &CVUniverse::GetDummyVar,
+           const bool is_true = false);
+
+  //==========================================================================
+  // Data members
+  //==========================================================================
+  std::string m_label;
+  std::string m_units;
+  Histograms  m_hists;
+  bool m_is_true;
+  // also, a pointer to CV universe function for Getting value (private)
+
+  //==========================================================================
+  // Functions
+  //==========================================================================
+  // Access
+  std::string Name() const { return m_label; }
+  int NBins() const { return m_hists.NBins(); }
+  int XMin() const  { return m_hists.XMin(); }
+  int XMax() const  { return m_hists.XMax(); }
+
+  // Get the variable's value
+  virtual double GetValue (const CVUniverse& universe, const int hadron_ID = -1) const;
+
+  // Histogram Initialization
+  template<typename T>
+  void InitializeAllHists(T systematic_univs, T systematic_univs_truth);
+  template<typename T>
+  void InitializeSidebandHists(T systematic_univs);
+  void InitializeStackedHists();
+  void InitializeDataHists();
+
+  // Write and Load MC Hists to/from a file
+  void WriteMCHists(TFile& fout) const;
+  void LoadDataHistsFromFile(TFile& fin);
+  void LoadMCHistsFromFile(TFile& fin, UniverseMap& error_bands);
+
+  // Histogram Access
+  template <typename T>
+  PlotUtils::MnvH1D* GetStackComponentHist(T type) const;
+
+  template <typename T>
+  TObjArray GetStackArray(T type) const ;
+
+  // Get Histograms from File
+  //void GetMCHists(TFile& fin);
+};
+#endif // __CINT__
+
+
+#include "VariableBase.cxx"
+
+
+#endif // VariableBase_h
