b48442d75d0ca23581ebe9abdf92a939412a6699
Ben Messerly <bamesserly@gmail.com>
2021-06-24 16:39:30 -0500

    Use latest processing to pull tuples from.

--- ccpion_common.h
+++ ccpion_common.h
@@ -8,8 +8,9 @@
 std::string GetPlaylistFile(std::string plist, bool is_mc, bool use_xrootd = true) {
 #ifndef __CINT__
   //const std::string processing_date = "20200713"; // new short tracking branches
-  const std::string processing_date = "20200920"; // new recoil energy branches
-  std::cout << "GetPlaylistFile: getting tuple playlist from " << processing_date << "processing\n";
+  //const std::string processing_date = "20200920"; // new recoil energy branches
+  const std::string processing_date = "20210307"; // nothing new, just an attempt at a new, complete processing.
+  std::cout << "GetPlaylistFile: getting tuple playlist from " << processing_date << " processing\n";
   const std::string is_mc_str = is_mc ? "mc" : "data";
   std::transform(plist.begin(), plist.end(), plist.begin(), ::toupper);
   std::string topdir = "/minerva/data/users/bmesserl/MECCCHpip_ana_plists/" + processing_date;
