a14ef9e8ecb0e33315a98ab940e70b53d21783dc
egranadosvazquez <granadosve2012@licifug.ugto.mx>
2021-11-17 10:33:46 -0600

    Update of tuple-utils/SubmitAuditAllPlaylistsToGrid.sh

--- tuple-utils/SubmitAuditAllPlaylistsToGrid.sh
+++ tuple-utils/SubmitAuditAllPlaylistsToGrid.sh
@@ -7,14 +7,14 @@ RELEASE_DIR="v22r1p1_UpMAD"
 unmerged_format_str="/%s_MAD%s_v49p1/"
 merged_format_str="MADtuplas/merged/%s/%s/%s/"
 
-#declare -a playlists=("ME1A" "ME1B" "ME1C" "ME1D" "ME1E" "ME1F" "ME1G" "ME1L" "ME1M" "ME1N" "ME1O" "ME1P")
-declare -a playlists=("ME1A" "ME1B" "ME1C" "ME1D")
+declare -a playlists=("ME1A" "ME1B" "ME1C" "ME1D" "ME1E" "ME1F" "ME1G" "ME1L" "ME1M" "ME1N" "ME1O" "ME1P")
+#declare -a playlists=("ME1A" "ME1B" "ME1C" "ME1D")
 declare -a datamc=("mc")
 
 for playlist in "${playlists[@]}"; do
   for dm in "${datamc[@]}"; do
     unmerged=$(printf "$unmerged_format_str" $dm $playlist)
     merged=$(printf "$merged_format_str" $MERGEDDATE $dm $playlist)
-    python $PLOTUTILSROOT/scripts/submitAuditToGrid.py --tool CCNuPionInc --release ${RELEASE_DIR} --unmerged_dir ${unmerged} --merged_dir $merged --${dm} --release_dir ${RELEASE_DIR}
+    python $PLOTUTILSSCRIPTS/submitAuditToGrid.py --tool MasterAnaDev --release ${RELEASE_DIR} --unmerged_dir ${unmerged} --merged_dir $merged --${dm} --release_dir ${RELEASE_DIR} --memory 500 --disk 15 
   done # data-mc
 done # playlists
