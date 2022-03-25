76ae23d1ddb186086b922302081092f39e57c94f
Ben Messerly <bamesserly@gmail.com>
2022-03-25 14:41:48 -0500

    Reorganize CVU (#37)
    No code change. Just reorganize.
    
    Previously I attempted to move functions into their own header files, but dependencies made it too complicated.
    
    Maybe later.
    
    Also want to do this with Cuts.

--- includes/CVUniverse.h
+++ includes/CVUniverse.h
@@ -1,127 +1,114 @@
 #ifndef CVUniverse_H
 #define CVUniverse_H
 
+#include <TVector3.h>
+
 #include "Binning.h"    // CCPi::GetBinning for ehad_nopi
 #include "Constants.h"  // CCNuPionIncConsts, CCNuPionIncShifts, Reco/TruePionIdx
 #include "PlotUtils/ChainWrapper.h"
 #include "PlotUtils/MinervaUniverse.h"
-#include <TVector3.h>
 
 class CVUniverse : public PlotUtils::MinervaUniverse {
  private:
   // Pion Candidates - clear these when SetEntry is called
   std::vector<RecoPionIdx> m_pion_candidates;
+
  public:
+#include "PlotUtils/MichelFunctions.h"
 #include "PlotUtils/MuonFunctions.h"
+#include "PlotUtils/RecoilEnergyFunctions.h"
 #include "PlotUtils/TruthFunctions.h"
 #include "PlotUtils/WeightFunctions.h"
-#include "PlotUtils/RecoilEnergyFunctions.h"
-#include "PlotUtils/MichelFunctions.h"
   // CTOR
   CVUniverse(PlotUtils::ChainWrapper* chw, double nsigma = 0);
 
   // DTOR
   virtual ~CVUniverse(){};
 
+  // Print arachne link
+  void PrintArachneLink() const;
+
+  // Dummy access for variable constructors
+  virtual double GetDummyVar() const;
+  virtual double GetDummyHadVar(const int x) const;
+
   // No stale cache!
   virtual void OnNewEntry() override { m_pion_candidates.clear(); }
 
-  // Get Branches and Calculate Quantities for the universe/event
-  // Muon Variables
-  virtual double GetPTmu() const;
-  //  virtual double GetPmu() const;
-  virtual double GetThetamuDeg() const;
-  virtual double GetPXmu() const;
-  virtual double GetPYmu() const;
-  virtual double GetPZmu() const;
+  // Get and set pion candidates
+  void SetPionCandidates(std::vector<RecoPionIdx> c);
+  std::vector<RecoPionIdx> GetPionCandidates() const;
+  int GetHighestEnergyPionCandidateIndex(const std::vector<int>& pions) const;
+  TruePionIdx GetHighestEnergyTruePionIndex() const;
 
+  //==============================================================================
+  // Analysis Variables
+  //==============================================================================
+  // muon
   virtual double GetEmuTrue() const;
-  virtual double GetPmuTrue() const;
+  virtual double GetPTmu() const;
   virtual double GetPTmuTrue() const;
+  virtual double GetPXmu() const;
   virtual double GetPXmuTrue() const;
+  virtual double GetPYmu() const;
   virtual double GetPYmuTrue() const;
+  virtual double GetPZmu() const;
   virtual double GetPZmuTrue() const;
+  virtual double GetPmuTrue() const;
+  virtual double GetThetamuDeg() const;
   virtual double GetThetamuTrue() const;
   virtual double GetThetamuTrueDeg() const;
 
-  // Event-wide Variables
+  // event-wide
+  virtual double GetEhad() const;
   virtual double GetEnu() const;
   virtual double GetQ2() const;
+  virtual double GetWexp() const;
   virtual double Getq0() const;
   virtual double Getq3() const;
-  virtual double GetWexp() const;
 
-  virtual double GetWexpTrue() const;
-  virtual double GetWgenie() const;
-
-  // Hadron (track) Variables
-  virtual double GetTpi(RecoPionIdx) const;
-  virtual double GetTpiMBR(RecoPionIdx) const;
-  virtual double GetThetapi(RecoPionIdx) const;
-  virtual double GetThetapiDeg(RecoPionIdx) const;
-  virtual double GetLLRScore(RecoPionIdx) const;
-  virtual double GetdEdxScore(RecoPionIdx) const;
-  virtual double GetEmichel(RecoPionIdx) const;
-
-  virtual double GetEnode0(RecoPionIdx) const;
-  virtual double GetEnode1(RecoPionIdx) const;
-  virtual double GetEnode01(RecoPionIdx) const;
-  virtual double GetEnode2(RecoPionIdx) const;
-  virtual double GetEnode3(RecoPionIdx) const;
-  virtual double GetEnode4(RecoPionIdx) const;
-  virtual double GetEnode5(RecoPionIdx) const;
-
-  virtual double GetPZpi(RecoPionIdx) const;
-  virtual double GetPXpi(RecoPionIdx) const;
-  virtual double GetPYpi(RecoPionIdx) const;
-  virtual double GetPpi(RecoPionIdx) const;
-
-  virtual double Gett(RecoPionIdx) const;
-   
-  virtual int GetNhadrons() const;
-  
+  // pion
   virtual double GetALR(RecoPionIdx) const;
-
   virtual double GetAdlerCosTheta(RecoPionIdx) const;
   virtual double GetAdlerPhi(RecoPionIdx) const;
-  virtual double GetpimuAngle(RecoPionIdx) const;
   virtual double GetPT(RecoPionIdx) const;
-
-  // With these truth hadron variables, SEE the warning in the .cxx
-  virtual double GetTpiTrue(TruePionIdx) const;
-  virtual std::vector<double> GetTpiTrueVec() const;
-  virtual double GetThetapiTrue(TruePionIdx) const;
-  virtual double GetThetapiTrueDeg(TruePionIdx) const;
-  virtual int GetPiChargeTrue(TruePionIdx) const;
-  virtual int GetNChargedPionsTrue() const;
-  virtual double GetAllTrackEnergyTrue() const;
-  virtual double GetAdlerCosThetaTrue(TruePionIdx) const;
-  virtual double GetAdlerPhiTrue(TruePionIdx) const;
-  virtual double GetpimuAngleTrue(TruePionIdx) const;
-  virtual double GetPTTrue(TruePionIdx) const;
-  virtual double GetALRTrue(TruePionIdx) const;
-
-  // Misc
-  virtual double GetLargestPrimProngSep() const;
-  virtual double GetLargestIsoProngSep() const;
-  virtual int GetNIsoProngs() const;
-  double GetTpiFResidual(const int hadron, const bool MBR = false) const;
-  double GetWexpFResidual() const;
-
-  // Some study variables
-  virtual int GetNAnchoredShortTracks() const;
-  virtual int GetNAnchoredLongTracks() const;
-  virtual double GetFitVtxX() const;
-  virtual double GetFitVtxY() const;
-  virtual double GetFitVtxZ() const;
-  virtual int GetTrackReconstructionMethod(RecoPionIdx) const;
-  virtual int GetNNodes(RecoPionIdx) const;
-
-  // Reco pion energy from dEdx tool.
+  virtual double GetPXpi(RecoPionIdx) const;
+  virtual double GetPYpi(RecoPionIdx) const;
+  virtual double GetPZpi(RecoPionIdx) const;
+  virtual double GetPpi(RecoPionIdx) const;
+  virtual double GetThetapi(RecoPionIdx) const;
+  virtual double GetThetapiDeg(RecoPionIdx) const;
+  virtual double GetTpi(RecoPionIdx) const;
+  virtual double GetTpiMBR(RecoPionIdx) const;
+  virtual double GetpimuAngle(RecoPionIdx) const;
+  virtual double Gett(RecoPionIdx) const;
   virtual double GetEpi(RecoPionIdx) const;
 
-  // Ehad new variables
-  virtual double GetEhad() const;
+  //==============================================================================
+  // Truth
+  //==============================================================================
+  virtual double GetALRTrue(TruePionIdx) const;
+  virtual double GetAdlerCosThetaTrue(TruePionIdx) const;
+  virtual double GetAdlerPhiTrue(TruePionIdx) const;
+  virtual double GetAllTrackEnergyTrue() const;
+  virtual double GetIntVtxXTrue() const;
+  virtual double GetIntVtxYTrue() const;
+  virtual double GetIntVtxZTrue() const;
+  virtual double GetPTTrue(TruePionIdx) const;
+  virtual double GetThetapiTrue(TruePionIdx) const;
+  virtual double GetThetapiTrueDeg(TruePionIdx) const;
+  virtual double GetTpiTrue(TruePionIdx) const;
+  virtual double GetWexpTrue() const;
+  virtual double GetWgenie() const;
+  virtual double GetpimuAngleTrue(TruePionIdx) const;
+  virtual int GetNChargedPionsTrue() const;
+  virtual int GetPiChargeTrue(TruePionIdx) const;
+  virtual std::vector<double> GetTpiTrueVec() const;
+
+  //==============================
+  // Ehad (GetErecoil) Variables
+  //==============================
+  // ehad and related variables
   virtual double GetCalRecoilEnergy() const;
   virtual double GetTrackRecoilEnergy() const;
   virtual double GetNonCalRecoilEnergy() const;
@@ -131,22 +118,65 @@ class CVUniverse : public PlotUtils::MinervaUniverse {
   virtual double GetCalRecoilEnergy_CCPiSpline() const;
   virtual double GetCalEpi(RecoPionIdx) const;
 
-  // Ehad old variables
+  // ehad old variables
   virtual double GetCalRecoilEnergy_CCIncSpline() const;
   virtual double GetCalRecoilEnergyNoPi_CCIncSpline() const;
 
-  // Ehad truth variables
+  // ehad truth variables
   virtual double GetEhadTrue() const;
   virtual double GetTpiTrueMatched(RecoPionIdx) const;
   virtual double GetEpiTrueMatched(RecoPionIdx) const;
   virtual double GetCalRecoilEnergyNoPiTrue() const;
 
-  // Dummy access for variable constructors
-  virtual double GetDummyVar() const;
-  virtual double GetDummyHadVar(const int x) const;
+  //==============================================================================
+  // Cuts, Systematics, Studies
+  //==============================================================================
+  virtual bool IsInHexagon(double x, double y, double apothem) const;
+  virtual bool IsInPlastic() const;
+  virtual bool leftlinesCut(const double a, const double x,
+                            const double y) const;
+  virtual bool rightlinesCut(const double a, const double x,
+                             const double y) const;
+  virtual double GetEmichel(RecoPionIdx) const;
+  virtual double GetEnode0(RecoPionIdx) const;
+  virtual double GetEnode01(RecoPionIdx) const;
+  virtual double GetEnode1(RecoPionIdx) const;
+  virtual double GetEnode2(RecoPionIdx) const;
+  virtual double GetEnode3(RecoPionIdx) const;
+  virtual double GetEnode4(RecoPionIdx) const;
+  virtual double GetEnode5(RecoPionIdx) const;
+  virtual double GetFitVtxX() const;
+  virtual double GetFitVtxY() const;
+  virtual double GetFitVtxZ() const;
+  virtual double GetLLRScore(RecoPionIdx) const;
+  virtual double GetLargestIsoProngSep() const;
+  virtual double GetLargestPrimProngSep() const;
+  virtual double GetTpiFResidual(const int hadron,
+                                 const bool MBR = false) const;
+  virtual double GetWexpFResidual() const;
+  virtual double GetdEdxScore(RecoPionIdx) const;
+  virtual int GetNAnchoredLongTracks() const;
+  virtual int GetNAnchoredShortTracks() const;
+  virtual int GetNIsoProngs() const;
+  virtual int GetNNodes(RecoPionIdx) const;
+  virtual int GetNhadrons() const;
+  virtual int GetTrackReconstructionMethod(RecoPionIdx) const;
 
-  // Calculate Quantities(Always MeV)
-  TVector3 AdlerAngle(int RefSystemDef, double dmumom, double dpimom, TVector3 NeuDir, TVector3 MuDir, TVector3 PiDir, double Enu) const;
+  //==============================================================================
+  // Weights
+  //==============================================================================
+  virtual double GetWeight() const;
+  virtual double GetDiffractiveWeight() const;
+  virtual double GetGenieWarpWeight() const;
+  virtual double GetLowQ2PiWarpWeight(double q2, std::string channel) const;
+  virtual double GetAnisoDeltaDecayWarpWeight() const;
+
+  //=============================================================================3
+  // Physics Calculations
+  //==============================================================================
+  TVector3 AdlerAngle(int RefSystemDef, double dmumom, double dpimom,
+                      TVector3 NeuDir, TVector3 MuDir, TVector3 PiDir,
+                      double Enu) const;
   double CalcQ2(const double Enu, const double Emu, const double Thetamu) const;
   double CalcWexp(const double Q2, const double Ehad) const;
   double Calcq0(const double Enu, const double Emu) const;
@@ -154,33 +184,6 @@ class CVUniverse : public PlotUtils::MinervaUniverse {
   double Calct(const double epi, const double emu, const double pzpi,
                const double pzmu, const double pxpi, const double pxmu,
                const double pypi, const double pymu) const;
-  // Functions to make fidvol cut
-  virtual bool rightlinesCut (const double a,const double x,const double y) const;
-  virtual bool leftlinesCut (const double a,const double x,const double y) const;
-  virtual bool IsInHexagon( double x, double y, double apothem ) const;
-  virtual bool IsInPlastic() const; 
-  virtual double GetIntVtxXTrue() const; 
-  virtual double GetIntVtxYTrue() const;
-  virtual double GetIntVtxZTrue() const; 
-  // Get Weight
-  virtual double GetWeight() const;
-  virtual double GetDiffractiveWeight() const;
-  // Warping
-  virtual double GetGenieWarpWeight() const;
-  virtual double GetLowQ2PiWarpWeight(double q2, std::string channel) const;
-  virtual double GetAnisoDeltaDecayWarpWeight() const;
-
-  // Get Highest Energy Pion from vector of candidates
-  int GetHighestEnergyPionCandidateIndex(
-      const std::vector<int>& pion_candidate_idxs) const;
-  TruePionIdx GetHighestEnergyTruePionIndex() const;
-
-  // Print arachne link
-  void PrintArachneLink() const;
-
-  // Pion Candidates
-  void SetPionCandidates(std::vector<RecoPionIdx> c);
-  std::vector<RecoPionIdx> GetPionCandidates() const;
 };
 
 #endif  // CVUniverse_H
