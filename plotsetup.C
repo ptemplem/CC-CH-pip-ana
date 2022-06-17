662f00f21010e5bf9b1f0569e788801a0eec0dc8
Philip Templeman <ptemplem@nd.edu>
2022-06-17 17:30:51 -0500

    Added plotsetup.C file for additional ROOT includes

---
+++ plotsetup.C
@@ -0,0 +1,7 @@
+{
+gInterpreter->AddIncludePath("${TOPDIR}/MAT/PlotUtils");
+gInterpreter->AddIncludePath("${TOPDIR}/MAT");
+gInterpreter->AddIncludePath("${TOPDIR}/MAT-MINERvA");
+gInterpreter->AddIncludePath("${TOPDIR}/MAT-MINERvA/universes");
+gInterpreter->AddIncludePath("${TOPDIR}/CC-CH-pip-ana/includes")
+}
