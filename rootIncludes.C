33d6ba9dd431ff5aa6fc406848e8b4e87ffd48f9
Philip Templeman <ptemplem@nd.edu>
2022-07-01 16:31:34 -0500

    Added rootIncludes.C

---
+++ rootIncludes.C
@@ -0,0 +1,9 @@
+{
+    gInterpreter->AddIncludePath( "${TOPDIR}/CC-CH-pip-ana/includes" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/calculators" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT/PlotUtils" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/universes" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/utilities" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/weighters" );
+}
