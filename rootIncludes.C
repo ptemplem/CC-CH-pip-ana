205167db0e2fe6b23e57be69e4ab228f15085e69
Philip Templeman <ptemplem@nd.edu>
2022-07-01 20:50:01 -0500

    Added MAT & MAT-MINERvA .so files to includes (possibly only a temp fix...)

--- rootIncludes.C
+++ rootIncludes.C
@@ -1,9 +1,13 @@
 {
     gInterpreter->AddIncludePath( "${TOPDIR}/CC-CH-pip-ana/includes" );
-    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/calculators" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/CC-CH-pip-ana" );
     gInterpreter->AddIncludePath( "${TOPDIR}/MAT" );
     gInterpreter->AddIncludePath( "${TOPDIR}/MAT/PlotUtils" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA" );
+    gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/calculators" );
     gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/universes" );
     gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/utilities" );
     gInterpreter->AddIncludePath( "${TOPDIR}/MAT-MINERvA/weighters" );
+    gSystem->Load( gSystem->ExpandPathName("$TOPDIR/CC-CH-pip-ana/includes/libMAT.so") );
+    gSystem->Load( gSystem->ExpandPathName("$TOPDIR/CC-CH-pip-ana/includes/libMAT-MINERvA.so") );
 }
